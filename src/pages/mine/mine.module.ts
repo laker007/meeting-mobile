f2ff195091ba38da947381bdfcea4b9cfe63d4e8
laker007 <838205692@qq.com>
2017-07-16 03:15:50 +0800

    lazy loading added

---
+++ src/pages/mine/mine.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MinePage } from './mine';
+@NgModule({
+  declarations: [MinePage],
+  imports: [IonicPageModule.forChild(MinePage)],
+})
+export class MinePageModule { }
---
+++ src/pages/mine/mine.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+@IonicPage()
+@Component({
+    selector: 'page-mine',
+    templateUrl: 'mine.html'
+})
+
+export class MinePage {
+
+}
