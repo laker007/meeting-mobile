900cf66bc2dc498d2791aa9cc42c8a1ec1dd6077
laker007 <838205692@qq.com>
2017-07-21 08:53:49 +0800

    some changes

---
+++ src/pages/login/login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+@IonicPage()
+@Component({
+    selector: 'page-login',
+    templateUrl: 'login.html'
+})
+
+export class LoginPage {
+
+}
