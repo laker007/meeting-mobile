b1c5816a1f8ad363e39399ecd01fcfbbba326c5d
laker007 <838205692@qq.com>
2017-07-21 18:01:02 +0800

    login signup page

--- src/pages/mine/login/login.ts
+++ src/pages/mine/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IonicPage, ViewController } from 'ionic-angular';
+import { HttpService } from '../../../providers/http.service';
 @IonicPage()
 @Component({
     selector: 'page-login',
@@ -7,11 +9,34 @@ import { IonicPage, ViewController } from 'ionic-angular';
 })
 
 export class LoginPage {
+    loginForm;
+
     constructor(
-        public viewCtrl: ViewController
-    ) { }
+        public viewCtrl: ViewController,
+        public http: HttpService
+    ) {
+        this.loginForm = new FormGroup({
+            username: new FormControl("", Validators.required),
+            password: new FormControl("", Validators.required)
+        })
+    }
 
     dismiss() {
         this.viewCtrl.dismiss();
+    }
+
+    login() {
+        let user;
+        user = {
+            username: this.loginForm.value.username,
+            password: this.loginForm.value.password
+        }
+        console.log(this.loginForm.value.username);
+        console.log(this.loginForm.value.password);
+        this.http.post('http://localhost:3001/api/user', user).subscribe(
+            data => {
+                console.log(data);
+            }
+        )
     }
 }
--- src/pages/mine/signup/signup.ts
+++ src/pages/mine/signup/signup.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IonicPage, ViewController } from 'ionic-angular';
+import { HttpService } from '../../../providers/http.service';
 @IonicPage()
 @Component({
     selector: 'page-signup',
@@ -7,10 +9,34 @@ import { IonicPage, ViewController } from 'ionic-angular';
 })
 
 export class SignupPage {
+
+    signupForm;
+
     constructor(
-        public viewCtrl: ViewController
-    ) { }
+        public viewCtrl: ViewController,
+        public http: HttpService,
+    ) {
+        this.signupForm = new FormGroup({
+            username: new FormControl("", Validators.required),
+            password: new FormControl("", Validators.required)
+        })
+    }
     dismiss() {
         this.viewCtrl.dismiss();
+    }
+
+    signup() {
+        let user;
+        user = {
+            username: this.signupForm.value.username,
+            password: this.signupForm.value.password
+        }
+        console.log(this.signupForm.value.username);
+        console.log(this.signupForm.value.password);
+        this.http.post('http://localhost:3001/api/user', user).subscribe(
+            data => {
+                console.log(data);
+            }
+        )
     }
 }
